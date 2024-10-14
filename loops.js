246e739c2e037d2b9fe5d7b711a3d6c67c0abd32
baldeepsinghx <bsinghchana@gmail.com>
2024-10-14 18:55:21 +0200

    Eerste opdracht gemaakt + HTML File geadd

--- loops.js
+++ loops.js
@@ -11,8 +11,13 @@
 // Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
 
+const teken = '*';
 
-
+console.log(teken);
+console.log(`*${teken.repeat(1)}`);
+console.log(`*${teken.repeat(2)}`);
+console.log(`*${teken.repeat(3)}`);
+console.log(`*${teken.repeat(4)}`);
 
 
 // ==========================================
